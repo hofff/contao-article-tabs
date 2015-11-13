2db256129eb6936b9195e6a311bcf5ea011af958
Oliver Hoff <oliver@hofff.com>
2015-11-13 11:17:08 +0100

    fix labels

--- contao-module/dca/tl_module.php
+++ contao-module/dca/tl_module.php
@@ -1,7 +1,7 @@
 <?php
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['bbit_mod_art_multi']['eval']['columnFields']['tabs_label'] = array(
-	'label'		=> &$GLOBALS['TL_LANG']['tl_content']['bbit_mod_art_tabs_label'],
+	'label'		=> &$GLOBALS['TL_LANG']['tl_module']['bbit_mod_art_tabs_label'],
 	'exclude'	=> true,
 	'inputType'	=> 'text',
 	'eval'		=> array(
